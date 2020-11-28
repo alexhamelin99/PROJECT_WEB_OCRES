37a95ea3811e6668102a79d0381262c969e95552
Alexandre <alexhamelin99@gmail.com>
2020-11-28 04:53:42 +0100

    la putain de comparaison

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -2,26 +2,45 @@ import React,{useState, useEffect} from 'react'
 import {
     LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
   } from 'recharts';
+  import Select from 'react-select';
+
 
 function Graph() {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [options, setOptions] = useState([])
+    const [firstLabel, setFirstLable] = useState('FR');
+    const [secondLable, setSecondLable] = useState('ITALY');
+    const [firstSelect, setFirstSelect] = useState(129);
+    const [secondSelect, setSecondSelect] = useState(152);
 
-    useEffect(() => {
         const runCall =async() => {
             let apiValue = await fetchData();
             const header = apiValue.split('\n').slice(0).map(line => (line.split(',')))[0].slice(4);
 
-            const frData = apiValue.split('\n').slice(0).map(line => (line.split(',')))[129].slice(4);
+            const firstPrep = apiValue.split('\n').slice(0).map(line => (line.split(',')))[firstSelect]
+            const secondPrep = apiValue.split('\n').slice(0).map(line => (line.split(',')))[secondSelect]
+
+            const firstLbl = firstPrep[1].toUpperCase();
+            const secondLbl = secondPrep[1].toUpperCase();
             
             const chartData =[];
             for (let i=0; i<header.length ; i++){
                 const newRow ={};
                 newRow.name = header[i];
-                newRow.data = parseInt(frData[i]);
+                newRow[firstLbl] = parseInt(firstPrep[i]);
+                newRow[secondLbl] = parseInt(secondPrep[i]);
                 chartData.push(newRow);
-                console.log(chartData);
             }
+            const countriesList = apiValue.split('\n').slice(1).map(line => (line.split(',').slice(0, 2)));
+            const countriesArr = [];
+            for (let i = 0; i < countriesList.length; i++) {
+            countriesArr.push({ label: countriesList[i].reverse().join(' : '), value: i + 1 })
+            }
+
+            setFirstLable(firstLbl);
+            setSecondLable(secondLbl);
+            setOptions(countriesArr)
             setData(chartData);
             setIsLoading(false);
             return null;
@@ -41,18 +60,28 @@ function Graph() {
                 return null;
             }
         }
-        runCall();
-    },[])
+        useEffect(() => {
+            runCall();
+          }, [firstSelect, secondSelect])
+        
+          const firstInput = (input) => {
+            setFirstSelect(input.value)
+          }
+        
+          const secondInput = (input) => {
+            setSecondSelect(input.value)
+          }
         return(
             <div className="chart-wrapper">
                 {isLoading ?
-                    <div>Loading...</div> :
+                    <div>loading...</div> :
+                    <div className="content-wrap">
                     <LineChart
-                        width={1000}
-                        height={700}
+                        width={800}
+                        height={500}
                         data={data}
                         margin={{
-                            top: 5, right: 30, left:20, bottom: 5
+                            top: 5, right: 30, left: 20, bottom: 5,
                         }}
                     >
                         <CartesianGrid strokeDasharray="3 3" />
@@ -60,9 +89,14 @@ function Graph() {
                         <YAxis />
                         <Tooltip />
                         <Legend />
-                        <Line type="monotone" dataKey="data" stroke="#8884d8" activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey={firstLabel} stroke="#8884d8" activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey={secondLable} stroke="#87d110" activeDot={{ r: 8 }} />
                     </LineChart>
-
+                    <div className="selector-wrap">
+                        <Select options={options} onChange={firstInput} placeholder="FRANCE" />
+                        <Select options={options} onChange={secondInput} placeholder="ITALY" />
+                    </div>
+                    </div>
                 }
             </div>
         );
